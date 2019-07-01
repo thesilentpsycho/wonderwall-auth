d5d06d80a5c9c7cbd07e512aa0a20fc0c544bbb1
Prateek Bhuwania <prateek@circles.asia>
2019-07-01 23:48:07 +0530

    validators

--- controllers/signup.go
+++ controllers/signup.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"bitbucket.org/libertywireless/wonderwall-auth/models"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/go-playground/validator.v9"
+)
 
 type AuthController interface {
 	SignUp(w http.ResponseWriter, r *http.Request)
@@ -11,7 +18,19 @@ type authController struct {
 }
 
 func (c *authController) SignUp(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Yo Bhuwania"))
+	decoder := json.NewDecoder(r.Body)
+	var form models.SignUpForm
+	err := decoder.Decode(&form)
+	if err != nil {
+		logrus.Errorln(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	v := validator.New()
+	err = v.Struct(form)
+	json.NewEncoder(w).Encode(err.(validator.ValidationErrors))
+
 }
 
 func (c *authController) SignIn(w http.ResponseWriter, r *http.Request) {
